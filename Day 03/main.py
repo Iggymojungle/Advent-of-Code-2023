2954645638c51a947bb331c3e6d362dbca91b12d
iggymojungle <iggymojungle@gmail.com>
2023-12-03 06:01:53 +0000

    Day 3 complete

--- Day 03/main.py
+++ Day 03/main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-
+from collections import defaultdict
 
 DATA = "data.txt"
 
@@ -22,12 +22,88 @@ def get_data():
     return data
             
 
-def part1(data):
-    return None
+DIRECTIONS = [
+    [0, -1],
+    [0, 1],
+    [-1, 0],
+    [1, 0],
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+]
 
+NOT_SYMBOL = ".1234567890"
+
+
+def part1(data):
+    total = 0
+    data = data.split("\n")
+    current_num = None
+    part_num = False
+    for linenum, line in enumerate(data):
+        for chrnum, character in enumerate(line):
+            if character.isnumeric():
+                if current_num is None:
+                    current_num = character
+                    part_num = False
+                else:
+                    current_num += character
+                
+                for direction in DIRECTIONS:
+                    if 0 <= linenum + direction[0] < len(data) and 0 <= chrnum + direction[1] < len(line):
+                        if data[linenum+direction[0]][chrnum+direction[1]] not in NOT_SYMBOL:
+                            part_num = True
+            elif current_num is not None:
+                if part_num:
+                    total += int(current_num)
+                current_num = None
+                part_num = False
+        if part_num:
+            total += int(current_num)
+        current_num = None
+        part_num = False
+    return total
+            
 
 def part2(data):
-    return None
+    total = 0
+    data = data.split("\n")
+    current_num = None
+    index = None
+    gears = defaultdict(int)
+    for linenum, line in enumerate(data):
+        for chrnum, character in enumerate(line):
+            if character.isnumeric():
+                if current_num is None:
+                    current_num = character
+                else:
+                    current_num += character
+                
+                for direction in DIRECTIONS:
+                    if 0 <= linenum + direction[0] < len(data) and 0 <= chrnum + direction[1] < len(line):
+                        if data[linenum+direction[0]][chrnum+direction[1]] == "*":
+                            index = str(linenum+direction[0]) + "," + str(chrnum+direction[1])
+                            
+            elif current_num is not None:
+                if index:
+                    if gears[index]:
+                        gears[index].append(int(current_num))
+                    else:
+                        gears[index] = [int(current_num)]
+                current_num = None
+                index = None
+        if index:
+            if gears[index]:
+                gears[index].append(int(current_num))
+            else:
+                gears[index] = [int(current_num)]
+        current_num = None
+        index = None
+    for gear in gears.values():
+        if len(gear) == 2:
+            total += gear[0] * gear[1]
+    return total
 
 
 def main():
