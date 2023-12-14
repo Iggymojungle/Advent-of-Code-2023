2fd5527f24ae4ea9d8dadc65e6120bd9a1b8d544
iggymojungle <iggymojungle@gmail.com>
2023-12-14 16:53:58 +0000

    Day 14 complete

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import time
+import copy
 
 
 DATA = "data.txt"
+CYCLES = 1000000000
 
 
 def get_data():
@@ -20,14 +22,64 @@ def get_data():
     with open(DATA, "r") as f:
         data = f.read()
     return data
-            
+
+
+def score(data):
+    max = len(data)
+    total = 0
+    for line_num, line in enumerate(data):
+        for character in line:
+            if character == "O":
+                total += max - line_num
+    return total
+
+
+def tilt_up(data):
+    for line_num, line in enumerate(data[1:]): # line_num offset by 1
+        for character_num, character in enumerate(line):
+            if data[line_num][character_num] == "." and character == "O":
+                data[line_num + 1][character_num] = "."
+                destination = 0
+                while line_num - destination > 0 and data[line_num - (destination + 1)][character_num] == ".":
+                    destination += 1
+                data[line_num - destination][character_num] = "O"
+    return data
+
 
 def part1(data):
-    return None
+    data = [list(i) for i in data.split("\n")]
+    data = tilt_up(data)
+    return score(data)
+
+
+def transpose(grid):
+    return [list(i) for i in list(zip(*grid))]
+
+
+def reverse(grid):
+    return list(reversed(grid))
+
+
+def cycle(data):
+    data = tilt_up(data)
+    data = transpose(tilt_up(transpose(data)))
+    data = reverse(tilt_up(reverse(data)))
+    return transpose(reverse(tilt_up(reverse(transpose(data)))))
 
 
 def part2(data):
-    return None
+    data = [list(i) for i in data.split("\n")]
+    cache = []
+    while data not in cache:
+        cache.append(copy.deepcopy(data))
+        data = cycle(data)
+
+    cycle_start = cache.index(data)
+    cycle_length = len(cache) - cycle_start
+    time_in_cycle = CYCLES - cycle_start - 1
+    grid_index = time_in_cycle % cycle_length + cycle_start
+    exact_grid = cache[grid_index + 1]
+    return score(exact_grid)
 
 
 def main():
