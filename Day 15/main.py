387f46e9437d40d55be94faa3b8da02fd03bfa6e
iggymojungle <iggymojungle@gmail.com>
2023-12-15 12:48:22 +0000

    Day 15 complete

--- Day 15/main.py
+++ Day 15/main.py
@@ -22,12 +22,46 @@ def get_data():
     return data
             
 
+def hash(string):
+    current = 0
+    for letter in string:
+        current += ord(letter)
+        current *= 17
+        current %= 256
+    return current
+
+
 def part1(data):
-    return None
+    data = data.split(",")
+    total = 0
+    for string in data:
+        total += hash(string)
+    return total
 
 
 def part2(data):
-    return None
+    data = data.split(",")
+    for thing_num, thing in enumerate(data):
+        if "=" in thing:
+            data[thing_num] = thing.split("=")
+        else:
+            data[thing_num] = [thing.split("-")[0]]
+            
+    boxes = [{} for _ in range(256)]
+    for instruction in data:
+        position = hash(instruction[0])
+        if len(instruction) == 2:
+            boxes[position][instruction[0]] = instruction[1]
+        elif len(instruction) == 1:
+            boxes[position][instruction[0]] = ""
+            del boxes[position][instruction[0]]
+        
+    total = 0
+    for box_num, box in enumerate(boxes):
+        for lens_num, lens in enumerate(box):
+            total += (box_num + 1) * (lens_num + 1) * int(box[lens])
+    return total
+
 
 
 def main():
