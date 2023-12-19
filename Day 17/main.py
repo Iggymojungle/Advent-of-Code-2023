19c568dcbc30069b3443e59b6e24351fd017ea76
iggymojungle <iggymojungle@gmail.com>
2023-12-19 16:13:31 +0000

    Days 17 and 18 complete

--- Day 17/main.py
+++ Day 17/main.py
@@ -11,6 +11,7 @@ DIRECTIONS = [
     [-1, 0],
 ]
 
+
 def get_data():
     os.chdir(os.path.dirname(__file__))
     if DATA not in os.listdir():
@@ -28,12 +29,10 @@ def get_data():
             
 
 class Node:
-    def __init__(self, position, value, grid):
+    def __init__(self, position, value, direction, grid):
         self.position = position
         self.connections = [[position[0] + direction[0], position[1] + direction[1]] for direction in DIRECTIONS if (0 <= position[0] + direction[0] < len(grid)) and (0 <= position[1] + direction[1] < len(grid[0]))]
         self.value = value
-        #self.last_node = {1:None, 2:None, 3:None}
-        #self.distance_to = {1:None, 2:None, 3:None}
         self.direction_from = None
         self.num_in_direction = 0
         self.last_node = None
@@ -41,30 +40,35 @@ class Node:
         self.final = False
     
     def __repr__(self) -> str:
-        #return str(self.position) + " " + str(self.value)
         return str(self.direction_from) + str(self.num_in_direction)
-    
-    #def __str__(self) -> str:
-        #return str(self.position)
         
 
 
 def part1(data):
     data = data.split("\n")
-    node_grid = [[Node([posy, posx], int(data[posy][posx]), data) for posx in range(len(data[posy]))] for posy in range(len(data))]
-    node_grid[0][0].distance_to = 0
-    to_visit = [node_grid[0][0]]
-    visited = []
+    node_grid = [[{tuple(direction) : [Node([posy, posx], int(data[posy][posx]), direction, data) for _ in range(3)] for direction in DIRECTIONS} for posx in range(len(data[posy]))] for posy in range(len(data))]
+    node_grid[0][0][(0, 1)][0].distance_to = 0
+    to_visit = [node_grid[0][0][(0, 1)][0]]
+    visited = set()
     while to_visit:
         to_visit = sorted(to_visit, key = lambda i : i.distance_to)
         current_node = to_visit.pop(0)
         current_node.final = True
-        visited.append(current_node)
+        visited.add(current_node)
         for connection in current_node.connections:
-            
-            connecting_node = node_grid[connection[0]][connection[1]]
-            dir_from = [connecting_node.position[0] - current_node.position[0], connecting_node.position[1] - current_node.position[1]]
-            if connecting_node not in visited and connecting_node not in to_visit and not (dir_from == current_node.direction_from and current_node.num_in_direction > 1):
+            dir_from = (connection[0] - current_node.position[0], connection[1] - current_node.position[1])
+            #print(dir_from)
+            if current_node.direction_from is not None:
+                if list(dir_from) == [i*-1 for i in current_node.direction_from]: # No doubling back!
+                    continue
+            if list(dir_from) == current_node.direction_from:
+                new_amount = current_node.num_in_direction + 1
+                if new_amount >= 3:
+                    continue
+            else:
+                new_amount = 0
+            connecting_node = node_grid[connection[0]][connection[1]][dir_from][new_amount]
+            if connecting_node not in visited and connecting_node not in to_visit and not (dir_from == current_node.direction_from and current_node.num_in_direction >= 2):
                 connecting_node.distance_to = current_node.distance_to + connecting_node.value
                 connecting_node.last_node = current_node
                 connecting_node.direction_from = [connecting_node.position[0] - current_node.position[0], connecting_node.position[1] - current_node.position[1]]
@@ -81,7 +85,7 @@ def part1(data):
                     original_direction = current_node.direction_from
                     connecting_node.direction_from = [connecting_node.position[0] - current_node.position[0], connecting_node.position[1] - current_node.position[1]]
                     if connecting_node.direction_from == current_node.direction_from:
-                        if current_node.num_in_direction > 1:
+                        if current_node.num_in_direction >= 2:
                             connecting_node.distance_to = original
                             current_node.direction_from = original_direction
                         else:
@@ -91,19 +95,26 @@ def part1(data):
                         connecting_node.num_in_direction = 0
                         connecting_node.last_node = current_node
                     
-    final_visited = []
-    current_node = node_grid[-1][-1]
-    while current_node is not None:
-        final_visited.append(current_node)
-        current_node = current_node.last_node
-    for line in node_grid:
-        for node in line:
-            if node in final_visited:
-                print("#",end="")
-            else:
-                print(".",end="")
-        print()
-    return node_grid[-1][-1].distance_to
+    final_nodes = []
+    final_distances = []
+    for direction in node_grid[-1][-1]:
+        for node in node_grid[-1][-1][direction]:
+            if node.distance_to is not None:
+                final_nodes.append(node)
+                final_distances.append(node.distance_to)
+    for current_node in final_nodes:
+        final_visited = []
+        while current_node is not None:
+            final_visited.append(current_node)
+            current_node = current_node.last_node
+        for line in node_grid:
+            for position in line:
+                node_in = False
+                for direction in position:
+                    for node in position[direction]:
+                        if node in final_visited:
+                            node_in = True
+    return min(final_distances)
 
 
 
@@ -112,7 +123,79 @@ def part1(data):
 
 
 def part2(data):
-    return None
+    data = data.split("\n")
+    node_grid = [[{tuple(direction) : [Node([posy, posx], int(data[posy][posx]), direction, data) for _ in range(10)] for direction in DIRECTIONS} for posx in range(len(data[posy]))] for posy in range(len(data))]
+    node_grid[0][0][(0, 1)][0].distance_to = 0
+    to_visit = [node_grid[0][0][(0, 1)][0]]
+    visited = set()
+    while to_visit:
+        if len(visited) % 1000 == 0:
+            print("Progress: " + str(len(visited)))
+        to_visit = sorted(to_visit, key = lambda i : i.distance_to) # TODO Implement insertion sort instead of this
+        current_node = to_visit.pop(0)
+        current_node.final = True
+        visited.add(current_node)
+        for connection in current_node.connections:
+            dir_from = (connection[0] - current_node.position[0], connection[1] - current_node.position[1])
+            if current_node.direction_from is not None:
+                if list(dir_from) == [i*-1 for i in current_node.direction_from]: # No doubling back!
+                    continue
+            if list(dir_from) == current_node.direction_from or (current_node.position == [0, 0] and current_node.last_node is None):
+                new_amount = current_node.num_in_direction + 1
+                if new_amount >= 10:
+                    continue
+            else:
+                if current_node.num_in_direction < 3:
+                    continue
+                new_amount = 0
+            connecting_node = node_grid[connection[0]][connection[1]][dir_from][new_amount]
+            if connecting_node not in visited and connecting_node not in to_visit and not (dir_from == current_node.direction_from and current_node.num_in_direction >= 9):
+                connecting_node.distance_to = current_node.distance_to + connecting_node.value
+                connecting_node.last_node = current_node
+                connecting_node.direction_from = [connecting_node.position[0] - current_node.position[0], connecting_node.position[1] - current_node.position[1]]
+                if connecting_node.direction_from == current_node.direction_from:
+                    connecting_node.num_in_direction = current_node.num_in_direction + 1
+                else:
+                    connecting_node.num_in_direction = 0
+                to_visit.append(connecting_node)
+            
+            elif connecting_node in to_visit:
+                original = connecting_node.distance_to
+                connecting_node.distance_to = min([connecting_node.distance_to, current_node.distance_to + connecting_node.value])
+                if connecting_node.distance_to != original:
+                    original_direction = current_node.direction_from
+                    connecting_node.direction_from = [connecting_node.position[0] - current_node.position[0], connecting_node.position[1] - current_node.position[1]]
+                    if connecting_node.direction_from == current_node.direction_from:
+                        if current_node.num_in_direction >= 9:
+                            connecting_node.distance_to = original
+                            current_node.direction_from = original_direction
+                        else:
+                            connecting_node.num_in_direction = current_node.num_in_direction + 1
+                            connecting_node.last_node = current_node
+                    else:
+                        connecting_node.num_in_direction = 0
+                        connecting_node.last_node = current_node
+                    
+    final_nodes = []
+    final_distances = []
+    for direction in node_grid[-1][-1]:
+        for node in node_grid[-1][-1][direction]:
+            if node.distance_to is not None:
+                final_nodes.append(node)
+                final_distances.append(node.distance_to)
+    for current_node in final_nodes:
+        final_visited = []
+        while current_node is not None:
+            final_visited.append(current_node)
+            current_node = current_node.last_node
+        for line in node_grid:
+            for position in line:
+                node_in = False
+                for direction in position:
+                    for node in position[direction]:
+                        if node in final_visited:
+                            node_in = True
+    return min(final_distances)
 
 
 def main():
--- Day 18/main.py
+++ Day 18/main.py
@@ -4,6 +4,13 @@ import time
 
 
 DATA = "data.txt"
+DIRECTIONS = {
+    "U" : [-1, 0],
+    "R" : [0, 1],
+    "L" : [0, -1],
+    "D" : [1, 0]
+}
+DIRECTION_NUMS = ["R", "D", "L", "U"]
 
 
 def get_data():
@@ -20,14 +27,100 @@ def get_data():
     with open(DATA, "r") as f:
         data = f.read()
     return data
-            
+
+
+def floodfill(grid, start_pos):
+    visited = set([tuple(start_pos)])
+    queue = [start_pos]
+    edge = False
+    while queue:
+        current = queue.pop(0)
+        for direction in DIRECTIONS.values():
+            cell = [current[0] + direction[0], current[1] + direction[1]] 
+            if not (cell[0] < 0 or cell[0] >= len(grid) or cell[1] < 0 or cell[1] >= len(grid[0])):
+                if tuple(cell) not in visited:
+                    if grid[cell[0]][cell[1]] == ".":
+                        visited.add(tuple(cell))
+                        queue.append(cell)
+                        if cell[0] in [0, len(grid)-1] or cell[1] in [0, len(grid[0])-1]:
+                            edge = True
+    for position in visited:
+        if edge:
+            grid[position[0]][position[1]] = "O"
+        else:
+            grid[position[0]][position[1]] = "#"
+    return grid
+
 
 def part1(data):
-    return None
+    data = [[i.split(" ")[0], int(i.split(" ")[1])] for i in data.split("\n")]
+    position = [0, 0]
+    dug = set([tuple(position)])
+    for instruction in data:
+        for _ in range(instruction[1]):
+            position[0] += DIRECTIONS[instruction[0]][0]
+            position[1] += DIRECTIONS[instruction[0]][1]
+            dug.add(tuple(position))
+    max_height = max([i[0] for i in dug]) + 1
+    max_length = max([i[1] for i in dug]) + 1
+    min_height = min([i[0] for i in dug])
+    min_length = min([i[1] for i in dug])
+    grid = [["." for _ in range(max_length - min_length)] for _ in range(max_height - min_height)]
+    for long in range(min_height, max_height):
+        for down in range(min_length, max_length):
+            if tuple([long, down]) in dug:
+                grid[long-min_height][down-min_length] = "#"
+            else:
+                grid[long-min_height][down-min_length] = "."
+    while "." in "".join(["".join(line) for line in grid]):
+        for linenum, line in enumerate(grid):
+            if "." in line:
+                grid = floodfill(grid, [linenum, line.index(".")])
+                break
+    total = 0
+    for line in grid:
+        total += line.count("#")
+    return total
+        
+
+def shoelace(vertices):
+  #A function to apply the Shoelace algorithm
+  numberOfVertices = len(vertices)
+  sum1 = 0
+  sum2 = 0
+  
+  for i in range(numberOfVertices-1):
+    sum1 = sum1 + vertices[i][0] * vertices[i+1][1]
+    sum2 = sum2 + vertices[i][1] * vertices[i+1][0]
+  
+  #Add xn.y1
+  sum1 = sum1 + vertices[numberOfVertices-1][0]*vertices[0][1]   
+  #Add x1.yn
+  sum2 = sum2 + vertices[0][0]*vertices[numberOfVertices-1][1]   
+  
+  area = abs(sum1 - sum2) / 2
+  return area
+   
 
 
 def part2(data):
-    return None
+    data = [i.split(" ")[2] for i in data.split("\n")]
+    data = [[DIRECTION_NUMS[int(i[7])], int(i[2:7], 16)] for i in data]
+    position = [0, 0]
+    corners = [list(position)]
+    for line in data:
+        position[0] += line[1] * DIRECTIONS[line[0]][0]
+        position[1] += line[1] * DIRECTIONS[line[0]][1]
+        corners.append(list(position))
+    area = shoelace(corners)
+    perimeter = 0
+    for corner_num, corner in enumerate(corners[:-1]):
+        perimeter += abs(corners[corner_num + 1][0] - corner[0])
+        perimeter += abs(corners[corner_num + 1][1] - corner[1])
+    area += perimeter / 2
+    area += 1
+    return int(area)
+    
 
 
 def main():
